71b002858420fa89483159f8572d9112c12f4816
PisamaTech <pisamatech@gmail.com>
2025-11-28 21:07:03 -0300

    Botón de Whatsapp crece 10% al hacer hover

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -7,10 +7,10 @@ export const WhatsAppButton = () => {
     <Link
       isExternal
       aria-label="Contactar por WhatsApp"
-      className="fixed bottom-4 right-4 z-50 bg-[#25D366] text-white p-3 rounded-full shadow-lg hover:bg-[#128C7E] transition-colors duration-300 flex items-center justify-center"
+      className="fixed bottom-4 right-4 z-50 bg-[#25D366] text-white p-3 rounded-full shadow-lg/50 hover:bg-[#128C7E] hover:scale-[1.10] transition-all duration-300 flex items-center justify-center"
       href="https://wa.me/59895961360"
     >
-      <WhatsappIcon size={32} />
+      <WhatsappIcon size={34} />
     </Link>
   );
 };
